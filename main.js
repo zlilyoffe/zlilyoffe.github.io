fd8743951b2cd288acbb7e6df335d20ae9311d12
zlilyoffe <zlilyoffe@gmail.com>
2022-07-04 18:59:39 +0300

    back ToTop

---
+++ main.js
@@ -0,0 +1,31 @@
+// $(window).scroll(function () {
+//   if ($(this).scrollTop() > 500) {
+//     $('.back-to-top').fadeIn();
+//   } else {
+//     $('.back-to-top').fadeOut();
+//   }
+// });
+
+// // scroll body to 0px on click
+// $('.back-to-top').click(function () {
+//   $('html, body').animate(
+//     {
+//       scrollTop: 0,
+//       easing: 'swing',
+//     },
+//     750
+//   );
+//   return false;
+// });
+
+// $('.testimonials-slider').bxSlider({
+//   nextSelector: '.tc-arrows .tc-arrow-right',
+//   prevSelector: '.tc-arrows .tc-arrow-left',
+//   nextText: "<i class='fa fa-angle-right'></i>",
+//   prevText: "<i class='fa fa-angle-left'></i>",
+//   pager: false,
+//   auto: true, // Boolean:  (true/false)
+//   pause: 5000, // Milliseconds before progressing to next slide automatically. Use a falsey value to disable.
+//   mode: 'vertical', // Choose fade, slide
+//   useCSS: false, // Boolean:  (true/false)
+// });
